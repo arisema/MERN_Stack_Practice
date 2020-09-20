fa5637a12beadf439cdd7dd02da1ac248457530f
Arisema Mezgebe <arisema.mezgebe@gmail.com>
2020-09-20 20:59:55 +0300

    Added node and mongodb containers and basic api configurations

---
+++ services/node_api/app.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const express = require('express');
+const path = require('path');
+const bodyparser = require('body-parser');
+const cors = require('cors');
+
+
+const app = express();
+
+app.use(bodyparser.urlencoded( { extended : true } ));
+app.use(bodyparser.json());
+app.use(cors());
+app.get('/', (_request, result) => {
+    result.send('Employee Mangement App');
+}
+);
+
+const HOST = process.env.HOST || '0.0.0.0';
+const PORT = process.env.PORT || 8888;
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
